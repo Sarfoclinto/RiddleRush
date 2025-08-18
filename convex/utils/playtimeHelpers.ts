3ca92c9ce77ae99b5fc3c7dc853dd14b9d321eb0
Sherlock <wolffighter00000@gmail.com>
2025-08-18 18:00:48 +0000

    code refactored

--- convex/utils/playtimeHelpers.ts
+++ convex/utils/playtimeHelpers.ts
@@ -1,4 +1,4 @@
-import { Id } from "../_generated/dataModel";
+import type { Id } from "../_generated/dataModel";
 import type { MutationCtx } from "../_generated/server";
 import { computeRiddlePointersFromIndex } from "./fns";
 
